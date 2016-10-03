05ac0797dad347dd8731368a94be267412d6fe16
Paulo Moreno <pcmoreno@yahoo.com>
2016-10-03 14:29:55 +0200

    Add files via upload
    mysql code that automatically inserts values into a table. Useful to create numeric scales.

---
+++ Creating a automatic scaled table.sql
@@ -0,0 +1,26 @@
+delimiter $$
+drop procedure if exists filltable$$
+
+
+
+create procedure filltable(IN records int, gap int, startingat int)
+Begin
+declare i int;
+set i = 0;
+Start transaction;
+while i < records do
+
+insert into origin
+VALUES(null, (startingat+gap*i),((startingat-1)+gap+gap*i));
+SET i = i + 1;
+end while;
+commit;
+END$$
+DELIMITER ;
+
+
+
+
+truncate origin;
+
+CALL filltable(10,25,1);
